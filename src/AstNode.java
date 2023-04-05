4bd09973d824fa249cf38ecf5a7599ceed600f9d
Koni vKxni <eventbeate@gmail.com>
2023-04-05 22:00:01 +0200

    astnodes

---
+++ src/AstNode.java
@@ -0,0 +1,13 @@
+
+public class AstNode {
+
+    public AstNode(String variableReference) {
+    }
+
+    public void addChild(AstNode statementNode) {
+    }
+
+    public void setValue(String value) {
+    }
+
+}
---
+++ src/AstNodeType.java
@@ -0,0 +1,14 @@
+
+public class AstNodeType {
+
+    public static final String BLOCK = null;
+    public static final String INTEGER_LITERAL = null;
+    public static final String STRING_LITERAL = null;
+    public static final String VARIABLE_REFERENCE = null;
+    public static final String BINARY_EXPRESSION = null;
+    public static final String DISPLAY_STATEMENT = null;
+    public static final String FUNCTION_DECLARATION = null;
+    public static final String VARIABLE_DECLARATION = null;
+    public static final String PROGRAM = null;
+
+}
